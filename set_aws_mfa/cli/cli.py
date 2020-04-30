7a3ca9f725af4841ff460fcfa2e37d5b6a3967f5
Yasuaki Shibata <shivaq777@gmail.com>
2020-04-30 11:11:08 +0900

    Mod:スイッチロールを考慮した関数に変更

--- set_aws_mfa/cli/cli.py
+++ set_aws_mfa/cli/cli.py
@@ -31,24 +31,34 @@ logger.propagate = False
 AWS_TMP_TOKEN = "~/.aws_tmp_token_for_set_mfa"
 
 
-def access_aws_with_mfa_code(selected_profile):
+def access_aws_with_mfa_code(selected_profile, role_profile):
     # 選択した profile の mfa の arn を用意するために、aws account id を取得
     mfa_arn = data_manager.get_mfa_arn(selected_profile)
 
     mfa_code = data_manager.get_mfa_code(selected_profile)
-
     sts_client = data_manager.get_sts_client(selected_profile)
-
     token_info = data_manager.get_token_info(selected_profile, sts_client, mfa_arn, str(mfa_code))
+    data_manager.create_a_file_to_set_env_var(token_info, selected_profile, role_profile)
 
-    data_manager.create_a_file_to_set_env_var(token_info)
+    print("\n\n認証情報を環境変数に設定します。 source {} を実行してください。\n\n".format(AWS_TMP_TOKEN))
+    # if selected_profile != role_profile:
+    #     print("mfa を使用し、かつスイッチロールを有効にするには、bash_profile に下記エイリアス設定をするか、--profile でロールを指定する必要があります ※ aws cli の仕様のため")
+    #     print("alias aws='aws --profile ${AWS_PROFILE:-""}'")
 
-    print("Please execute 'source {}'".format(AWS_TMP_TOKEN))
+
+def select_role(selected_profile):
+    # 設定情報取得
+    profile_obj_list = data_manager.get_profile_obj_list()
+    role_for_the_profile_list = data_manager.get_role_list_for_a_profile(selected_profile, profile_obj_list)
+    # ロール関連アクションを実行
+    role_action_num = int(data_manager.get_role_action(selected_profile, role_for_the_profile_list))
+    return data_manager.switch_actions_for_role(selected_profile, role_action_num, role_for_the_profile_list)
 
 
 def access_aws_after_reset_aws_account_id(selected_profile):
     data_manager.reset_aws_account_id(selected_profile)
-    access_aws_with_mfa_code(selected_profile)
+    role_profile = select_role(selected_profile)
+    access_aws_with_mfa_code(selected_profile, role_profile)
 
 
 def start_set_aws_mfa():
@@ -56,19 +66,9 @@ def start_set_aws_mfa():
     validate.check_aws_config_existence()
     validate.check_aws_credentials_existence()
 
-    # 設定情報取得
-    profile_obj_list = data_manager.get_profile_obj_list()
-    role_profile = data_manager.get_role_profile(profile_obj_list)
-
     # profile 選択のためのユーザー入力要求
     selected_profile = data_manager.get_selected_profile()
-    role_for_the_profile_list = data_manager.get_role_list_for_a_profile(selected_profile, profile_obj_list)
-    print(role_for_the_profile_list)
-    # TODO: profile と関連するロールを取得する
-    # TODO: 関連ロールがない場合、入力を促す
-    # TODO: role の選択を促す
-    # TODO: 選択したロールで、認証をする
-    # TODO: 該当ロールでの認証に失敗したら、ロールのデータを更新、削除を促す
-    # TODO: 再度認証を試みる
+    # スイッチロールに使う profile もしくは ソースとなる IAM ユーザープロファイルを取得
+    role_profile = select_role(selected_profile)
 
-    # access_aws_with_mfa_code(selected_profile)
+    access_aws_with_mfa_code(selected_profile, role_profile)
